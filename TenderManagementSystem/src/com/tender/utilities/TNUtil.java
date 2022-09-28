832cac44e5ef794bb64caa10605e48eaa8b89c79
sumitraghavwork <sumitraghavwork@gmail.com>
2022-09-28 20:39:28 +0530

    day-2

---
+++ TenderManagementSystem/src/com/tender/utilities/TNUtil.java
@@ -0,0 +1,39 @@
+package com.tender.utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TNUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+	
+	public Connection getConnection() {
+		
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:mysql://localhost:3306/tender";
+		
+		try {
+			conn = DriverManager.getConnection(url, "root", "root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+
+}
